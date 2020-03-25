8c4690f8b713c8bc74d6dc7e3d786b9a5f3c5bbf
robyn <34239417+robynph@users.noreply.github.com>
2020-03-25 19:00:06 -0400

    updated to worker example

--- app.py
+++ app.py
@@ -1,24 +1,8 @@
 # in app.py
 
-import os
+from rq import Queue
+from worker import conn
+from utils import count_words_at_url
 
-app.conf.update(BROKER_URL=os.environ['REDIS_URL'],
-                CELERY_RESULT_BACKEND=os.environ['REDIS_URL'])
-
-def make_celery(app):
-	# set redis url vars
-	app.config['CELERY_BROKER_URL'] = environ.get('REDIS_URL', 'redis://localhost:6379/0')
-    app.config['CELERY_RESULT_BACKEND'] = app.config['CELERY_BROKER_URL']
-    # create context tasks in celery
-    celery = Celery(app.import_name, broker=app.config['CELERY_BROKER_URL'])
-    celery.conf.update(app.config)
-    TaskBase = celery.Task
-    class ContextTask(TaskBase):
-        abstract = True
-        def __call__(self, *args, **kwargs):
-            with app.app_context():
-                return TaskBase.__call__(self, *args, **kwargs)
-    celery.Task = ContextTask
-    return celery
-
-celery = make_celery(current_app)
+q = Queue(connection=conn)
+result = q.enqueue(count_words_at_url, 'http://heroku.com')
