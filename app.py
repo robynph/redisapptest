b6ae2d57b0e664e0733e0b0aa6fe6b4f0a6823e4
robyn <34239417+robynph@users.noreply.github.com>
2020-03-25 19:45:50 -0400

    updated

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
 # in app.py
 
+from flask import Flask, jsonify, url_for, request, redirect
+
 from rq import Queue
 from worker import conn
 from utils import count_words_at_url
 
-q = Queue(connection=conn)
-result = q.enqueue(count_words_at_url, 'http://heroku.com')
+app = Flask(__name__)
+
+
+@app.route("/count/< URL >", methods=["POST"])
+def count(URL) -> str:
+    print('task in app.py')
+    q = Queue(connection=conn)
+    result = q.enqueue(count_words_at_url, {URL})
+    return jsonify("result": result)
+
+if __name__ == "__main__":
+    app.run(debug=True)
