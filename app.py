ab59a7d2927e73340e1a606de4120b2adcdbfcf6
robyn <34239417+robynph@users.noreply.github.com>
2020-03-25 20:36:27 -0400

    updated

--- app.py
+++ app.py
@@ -7,7 +7,10 @@ from worker import conn
 from utils import count_words_at_url
 
 app = Flask(__name__)
-
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
 
 @app.route("/count", methods=["POST"])
 def count():
